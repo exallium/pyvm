e13fa67f646d4b8f2ce7d1d146dca1cac35bdbaf
Alex Hart <alex.hart658@gmail.com>
2011-07-06 20:47:20 -0400

    Implement label parsing as prefix for 1pass
    This label parsing is considered a part of the
    first pass, but must be done explicitly before hand.
    This allows for labels to appear in code before they are
    declared.  The way the software is currently implemented,
    unknown labels will be dealt with in either the second phase
    of the first pass or the second pass, depending on where they
    lie.

--- assembler.py
+++ assembler.py
@@ -13,21 +13,49 @@ def main():
     instr_strings = fin.read()
     fin.close()
     instr_strings = instr_strings.split('\n')
-    
-    # The last one is garbage and there's no point in processing it
-    instr_strings = instr_strings[:len(instr_strings)-1]
 
+    instr_strings = instr_strings[:len(instr_strings) - 1]
+    
     # We capitalize every instruction, and then split each list by ' '
     for (index, string) in enumerate(instr_strings):
+        if string.find(';') != -1:
+            string = string[:string.find(';')]
+
+        instr_strings[index] = [instr for instr in instr_strings[index] if instr != ';']
         instr_strings[index] = string.upper().split(' ')
+        instr_strings[index] = [instr for instr in instr_strings[index] if instr != '']
 
     # We don't check whether or not END exists here.  We find it on
     # the go. As soon as we do, we're done.
     ip = 0
 
-    #First pass is where the magic happens
-    #Conversions are made and assembler directives are handled
+    # First thing we do is grab all of the labels.
+    # We compile this list and spit them into their corresponding locations.
+    # Label declarations are removed, label references are replaced with their
+    # corresponding location
+    labels = {}
+    for string in instr_strings:
+        for (index, item) in enumerate(string):
+            if item[len(item) - 1] == ':':  # We have a label
+                item = item.rstrip(':')
+                if (item[0].isdigit() or instr_dir.get(item, None)
+                        or reg_dir.get(item, None)):
+                    print '1pass: e: ip=%s: %s is invalid label' % (ip, item)
+                    return
+                if labels.get(item, None):
+                    print '1pass: e: ip=%s: %s is duplicate label' % (ip, item)
+                    return
+                labels[item] = ip
+                string.remove('%s:' % item)
+            elif labels.get(item, None):
+                string[index] = '$%s' % labels.get(item)
+            elif item[0] == '$':  # We have a memory address
+                ip = ip + 2
+            else:
+                ip = ip + 1
 
+    instr_strings = [string for string in instr_strings if string]
+    
     # Second Pass
     fout = open(sys.argv[2], 'w')
     
@@ -40,8 +68,8 @@ def main():
             fout.write("%s %s " % (ip, instr_dir[string[0]]['opcode']))
             ip = ip + 1
             if len(string) - 1 != instr_dir[string[0]]['nops']:
-                print ('2pass: e: irregular number of ops for %s'
-                    % string)
+                print ('2pass: e: ip=%s: irregular number of ops for %s'
+                    % (ip, string))
                 return
             for strr in string[1:]:
                 try:
@@ -56,15 +84,17 @@ def main():
                         item = int(strr[1:], 16)
                         fout.write('%s %s' % (item >> 8, item & 0xFF))
                     else:
-                        print ('2pass: e: invalid operand prefix: %s'
-                                % strr[0])
+                        print ('2pass: e: ip=%s: invalid operand prefix: %s'
+                                % (ip, strr[0]))
                         return
             if ip - prev_ip != instr_dir[string[0]]['nbytes']:
-                print "2pass: e: invalid byte count for %s" % strr[0]
+                print "2pass: e: ip=%s: invalid byte count for %s" % (ip,
+                        strr[0])
+                print '%s' % prev_ip
             fout.write('\n')
             
     except KeyError:
-        print 'second pass: error: %s: invalid.' % string[0]
+        print 'second pass: error: ip=%s: %s: invalid.' % (ip, string[0])
 
     fout.close()
 
--- test/vm_test.as
+++ test/vm_test.as
@@ -1,6 +1,8 @@
-ldi ax #3
-ldi bx #2
-add ax bx
-ldm si $56DE
-mov ax si
+    ldi     ax #3
+    ldi bx #2       ; This is a comment and should be ignored.
+    add ax bx
+label:
+    ldm si $56DE
+    mov ax si
+    bra label
 END
